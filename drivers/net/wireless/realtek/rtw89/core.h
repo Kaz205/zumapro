065c90d3663d4948af73b6ba5f8e2ee493c91aec
Ping-Ke Shih <pkshih@realtek.com>
2023-12-13 08:50:51 +0800

    UPSTREAM: wifi: rtw89: load RFK log format string from firmware file
    To debug RFK (RF calibration) in firmware, it sends log via firmware C2H
    events to driver with string format ID and four arguments. Load formatted
    string from firmware file, and the string ID can get back its string. Then,
    use regular print format to show the message.
    
    This firmware element layout looks like
    
        +============================================+
        |  elm ID  | elm size | version  |           |
        +----------+----------+----------+-----------+
        |                     | nr |rsvd |rfk_id|rsvd|
        +--------------------------------------------+
        | offset[] (__le16 * nr)                     |
        | ...                                        |
        +--------------------------------------------+
        | formatted string with null termintor (*nr) |
        | ...                                        |
        +============================================+
    
     * a firmware file can contains more than one elements with this element ID
       named RTW89_FW_ELEMENT_ID_RFKLOG_FMT (19), because many RFK needs its
       own formatted strings, so add 'rfk_id' to know it belongs to which RFK.
     * the 'formatted string' just follow 'offset[]' without padding to align
       32bits.
    
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/20231213005054.10568-4-pkshih@realtek.com
    (cherry picked from commit 7a9192eecf2704ac10d91ca677433148e1720291)
    
    BUG=b:371471747
    TEST=verify suite:wifi_matfunc and suite:wifi_perf on dedede with 8852BT.
    
    Change-Id: I7288aa62a6eae21448448271abe2160b201102ed
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5965466
    Reviewed-by: Ruth Mekonnen <rmekonnen@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Ruth Mekonnen <rmekonnen@google.com>
    Tested-by: Ruth Mekonnen <rmekonnen@google.com>

--- drivers/net/wireless/realtek/rtw89/core.h
+++ drivers/net/wireless/realtek/rtw89/core.h
@@ -17,6 +17,7 @@ struct rtw89_pci_info;
 struct rtw89_mac_gen_def;
 struct rtw89_phy_gen_def;
 struct rtw89_fw_txpwr_track_cfg;
+struct rtw89_phy_rfk_log_fmt;
 
 extern const struct ieee80211_ops rtw89_ops;
 
@@ -4228,6 +4229,7 @@ struct rtw89_fw_elm_info {
 	struct rtw89_phy_table *rf_radio[RF_PATH_MAX];
 	struct rtw89_phy_table *rf_nctl;
 	struct rtw89_fw_txpwr_track_cfg *txpwr_trk;
+	struct rtw89_phy_rfk_log_fmt *rfk_log_fmt;
 };
 
 struct rtw89_fw_info {
