7d611a3ef872646bca17a54440100dacb4646f11
Ching-Te Ku <ku920601@realtek.com>
2024-03-12 09:37:19 +0800

    BACKPORT: wifi: rtw89: coex: Add TDMA slot parameter setting version 7
    In order to packet up the slots information, the TLV header has updated.
    TDMA slot parameters also use the TLV header to packet up to H2C, so
    upgrade to version 7.
    
    Signed-off-by: Ching-Te Ku <ku920601@realtek.com>
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>
    Signed-off-by: Kalle Valo <kvalo@kernel.org>
    Link: https://msgid.link/20240312013721.17452-5-pkshih@realtek.com
    (cherry picked from commit a7d6f8d0c6d23e5c03ee7df88725e23597c8b6de)
    
    Conflicts:
       drivers/net/wireless/realtek/rtw89/coex.h
    	(contextual conflict)
    
    BUG=b:351289055
    TEST=verify suite:wifi_matfunc and suite:wifi_perf on dedede with 8852B.
    
    Change-Id: I02b8216a52b55d4c953845deb514ea9dff2b5cb9
    Signed-off-by: Zong-Zhe Yang <kevin_yang@realtek.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5850565
    Tested-by: Andrzej Ostruszka <andrzejo@google.com>
    Reviewed-by: David Ruth <druth@chromium.org>
    Commit-Queue: David Ruth <druth@chromium.org>
    Reviewed-by: Andrzej Ostruszka <andrzejo@google.com>

--- drivers/net/wireless/realtek/rtw89/coex.h
+++ drivers/net/wireless/realtek/rtw89/coex.h
@@ -8,6 +8,7 @@
 #include "core.h"
 
 #define BTC_H2C_MAXLEN 2020
+#define BTC_TLV_SLOT_ID_LEN_V7 1
 
 enum btc_mode {
 	BTC_MODE_NORMAL,
@@ -268,6 +269,53 @@ static inline u8 rtw89_btc_path_phymap(struct rtw89_dev *rtwdev,
 				       enum rtw89_rf_path path)
 {
 	return rtw89_btc_phymap(rtwdev, phy_idx, BIT(path));
+}
+
+static inline
+void _slot_set_le(struct rtw89_btc *btc, u8 sid, __le16 dura, __le32 tbl, __le16 type)
+{
+	if (btc->ver->fcxslots == 1) {
+		btc->dm.slot.v1[sid].dur = dura;
+		btc->dm.slot.v1[sid].cxtbl = tbl;
+		btc->dm.slot.v1[sid].cxtype = type;
+	} else if (btc->ver->fcxslots == 7) {
+		btc->dm.slot.v7[sid].dur = dura;
+		btc->dm.slot.v7[sid].cxtype = type;
+		btc->dm.slot.v7[sid].cxtbl = tbl;
+	}
+}
+
+static inline
+void _slot_set(struct rtw89_btc *btc, u8 sid, u16 dura, u32 tbl, u16 type)
+{
+	_slot_set_le(btc, sid, cpu_to_le16(dura), cpu_to_le32(tbl), cpu_to_le16(type));
+}
+
+static inline
+void _slot_set_dur(struct rtw89_btc *btc, u8 sid, u16 dura)
+{
+	if (btc->ver->fcxslots == 1)
+		btc->dm.slot.v1[sid].dur = cpu_to_le16(dura);
+	else if (btc->ver->fcxslots == 7)
+		btc->dm.slot.v7[sid].dur = cpu_to_le16(dura);
+}
+
+static inline
+void _slot_set_type(struct rtw89_btc *btc, u8 sid, u16 type)
+{
+	if (btc->ver->fcxslots == 1)
+		btc->dm.slot.v1[sid].cxtype = cpu_to_le16(type);
+	else if (btc->ver->fcxslots == 7)
+		btc->dm.slot.v7[sid].cxtype = cpu_to_le16(type);
+}
+
+static inline
+void _slot_set_tbl(struct rtw89_btc *btc, u8 sid, u32 tbl)
+{
+	if (btc->ver->fcxslots == 1)
+		btc->dm.slot.v1[sid].cxtbl = cpu_to_le32(tbl);
+	else if (btc->ver->fcxslots == 7)
+		btc->dm.slot.v7[sid].cxtbl = cpu_to_le32(tbl);
 }
 
 #endif
