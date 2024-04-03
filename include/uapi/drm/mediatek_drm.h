8ed33a2d7e155ac6bad557bb53e8186b09d561dd
Jason-JH.Lin <jason-jh.lin@mediatek.com>
2024-04-03 18:26:53 +0800

    BACKPORT: FROMLIST: drm/mediatek/uapi: Add DRM_MTK_GEM_CREATE_RESTRICTED flag
    Add DRM_MTK_GEM_CREATE_RESTRICTED flag for the user space to tell the
    kernel space this dma buffer is restricted.
    The user space can store this flag into the private data of the dma
    buffer after allocating.
    
    A restricted buffer is used to store the secure video content to support
    secure video path feature.
    
    Signed-off-by: Jason-JH.Lin <jason-jh.lin@mediatek.com>
    Signed-off-by: Hsiao Chien Sung <shawn.sung@mediatek.com>
    (am from https://patchwork.kernel.org/project/linux-mediatek/patch/20240525232928.5524-3-jason-jh.lin@mediatek.com/)
    
    Conflicts:
      include/uapi/drm/mediatek_drm.h
        - #define DRM_MTK_GEM_CREATE_RESTRICTED    0x01
        + #define DRM_MTK_GEM_CREATE_RESTRICTED    0x02
    
    BUG=b:248609774
    TEST=emerge-geralt sys-kernel/chromeos-kernel-6_1
    
    Change-Id: Iddfa2566447f6170182bbd518a06cbabca2025e1
    Signed-off-by: Hsiao Chien Sung <shawn.sung@mediatek.corp-partner.google.com>
    Signed-off-by: Jason-jh Lin <jason-jh.lin@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5173265
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- include/uapi/drm/mediatek_drm.h
+++ include/uapi/drm/mediatek_drm.h
@@ -54,6 +54,7 @@ struct drm_mtk_gem_map_off {
 
 #define DRM_MTK_GEM_CREATE		0x00
 #define DRM_MTK_GEM_MAP_OFFSET		0x01
+#define DRM_MTK_GEM_CREATE_RESTRICTED	0x02
 
 #define DRM_IOCTL_MTK_GEM_CREATE	DRM_IOWR(DRM_COMMAND_BASE + \
 		DRM_MTK_GEM_CREATE, struct drm_mtk_gem_create)
