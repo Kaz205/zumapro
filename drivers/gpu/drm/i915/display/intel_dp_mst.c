f712bcb3e03fa4e706c727504c4423ce89843528
Charlton Lin <charlton.lin@intel.com>
2024-05-02 16:41:20 -0700

    Revert "CHROMIUM: drm/i915: Allow bigjoiner for MST"
    This reverts commit 87776f3b992be1daf9ec1a4e6bb675d0d10edcc9.
    
    Reason for revert:
    Take upstream version
    
    Original change's description:
    >CHROMIUM: drm/i915: Allow bigjoiner for MST
    >
    >We need bigjoiner support with MST functionality
    >for MST monitor resolutions > 5K to work.
    >Adding support for the same.
    >
    >v2: Addressed review comments from Jani.
    >Revert rejection of MST bigjoiner modes and add
    >functionality
    >
    >v3: Fixed pipe_mismatch WARN for mst_master_transcoder
    >Credits-to: Manasi Navare <navaremanasi@chromium.org>
    >
    >BUG=b:298042667
    >UPSTREAM-TASK=b:326336997
    >TEST= 6K resolution on MST Dell DP 2.1 monitor
    >
    >Acked-by: Manasi Navare <navaremanasi@chromium.org
    >Change-Id: I882d6cb5f4f6dd0007ff9d84e2e7be0b03565dbb
    >Signed-off-by: Vidya Srinivas <vidya.srinivas@intel.com>
    >Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5078757
    >Tested-by: Manasi Navare <navaremanasi@chromium.org>
    >Reviewed-by: Manasi Navare <navaremanasi@chromium.org>
    Signed-off-by: Charlton Lin <charlton.lin@intel.com>
    
    BUG=b:308455498
    TEST=Verify Asus PG32UQXR 4K160 mode on Rex using Big Joiner
    
    Change-Id: Ic7b41652ad197bcd9b31fabea926af4a76c881f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5514464
    Commit-Queue: Manasi Navare <navaremanasi@chromium.org>
    Reviewed-by: Manasi Navare <navaremanasi@chromium.org>
    Tested-by: Charlton Lin <charlton.lin@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_mst.c
+++ drivers/gpu/drm/i915/display/intel_dp_mst.c
@@ -525,7 +525,6 @@ static int intel_dp_mst_compute_config(struct intel_encoder *encoder,
 	struct drm_i915_private *dev_priv = to_i915(encoder->base.dev);
 	struct intel_atomic_state *state = to_intel_atomic_state(conn_state->state);
 	struct intel_dp_mst_encoder *intel_mst = enc_to_mst(encoder);
-	struct intel_crtc *crtc = to_intel_crtc(pipe_config->uapi.crtc);
 	struct intel_dp *intel_dp = &intel_mst->primary->dp;
 	const struct intel_connector *connector =
 		to_intel_connector(conn_state->connector);
@@ -541,10 +540,6 @@ static int intel_dp_mst_compute_config(struct intel_encoder *encoder,
 
 	if (adjusted_mode->flags & DRM_MODE_FLAG_DBLSCAN)
 		return -EINVAL;
-
-	if (intel_dp_need_bigjoiner(intel_dp, adjusted_mode->crtc_hdisplay,
-				    adjusted_mode->crtc_clock))
-		pipe_config->bigjoiner_pipes = GENMASK(crtc->pipe + 1, crtc->pipe);
 
 	pipe_config->sink_format = INTEL_OUTPUT_FORMAT_RGB;
 	pipe_config->output_format = INTEL_OUTPUT_FORMAT_RGB;
@@ -1336,6 +1331,12 @@ intel_dp_mst_mode_valid_ctx(struct drm_connector *connector,
 	 *   corresponding link capabilities of the sink) in case the
 	 *   stream is uncompressed for it by the last branch device.
 	 */
+	if (mode_rate > max_rate || mode->clock > max_dotclk ||
+	    drm_dp_calc_pbn_mode(mode->clock, min_bpp << 4) > port->full_pbn) {
+		*status = MODE_CLOCK_HIGH;
+		return 0;
+	}
+
 	if (mode->clock < 10000) {
 		*status = MODE_CLOCK_LOW;
 		return 0;
@@ -1349,10 +1350,8 @@ intel_dp_mst_mode_valid_ctx(struct drm_connector *connector,
 	if (intel_dp_need_bigjoiner(intel_dp, mode->hdisplay, target_clock)) {
 		bigjoiner = true;
 		max_dotclk *= 2;
-	}
 
-	if (mode_rate > max_rate || mode->clock > max_dotclk ||
-	    drm_dp_calc_pbn_mode(mode->clock, min_bpp << 4) > port->full_pbn) {
+		/* TODO: add support for bigjoiner */
 		*status = MODE_CLOCK_HIGH;
 		return 0;
 	}
@@ -1399,7 +1398,7 @@ intel_dp_mst_mode_valid_ctx(struct drm_connector *connector,
 		return 0;
 	}
 
-	*status = intel_mode_valid_max_plane_size(dev_priv, mode, bigjoiner);
+	*status = intel_mode_valid_max_plane_size(dev_priv, mode, false);
 	return 0;
 }
 
